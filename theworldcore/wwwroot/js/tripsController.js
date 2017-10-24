fbb4484dab2e9561e145ab618c61fda08ae64e11
andkir <kyrienko@gmail.com>
2017-10-24 15:10:49 +0300

    added angularjs

---
+++ theworldcore/wwwroot/js/tripsController.js
@@ -0,0 +1,25 @@
+﻿(function () {
+
+    angular.module("app-trips").controller("tripsController", tripsController);
+
+    function tripsController() {
+        var vm = this;
+
+        vm.trips = [{
+            name: 'USA Trip',
+            created: new Date()
+        },
+            {
+                name: 'Europe Trip',
+                created: new Date()
+            }];
+
+        vm.newTrip = {};
+
+        vm.addTrip = function () {
+            vm.trips.push(vm.newTrip);
+            vm.newTrip = {};
+        }
+    }
+
+})();
